9ea673d87fac2cbe2d2ae4d4dbde38e282b4a15b
CanucksAndBolts <61293710+CanucksAndBolts@users.noreply.github.com>
2024-02-24 12:51:09 -0500

    Add files via upload

---
+++ robot code/everythingSoFar/2.24.24/autos/Autos.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.autos;
+
+//import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.motors.Drivetrain;
+import frc.robot.subsystems.motors.Intake;
+import frc.robot.subsystems.motors.Shooter;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.autos.autoMinCommands.autoShootCommand;
+import frc.robot.autos.autoMinCommands.driveOutStartCommand;
+import frc.robot.autos.testAutosCommands.doNothing;
+import frc.robot.autos.testAutosCommands.testAutoCommand;
+
+public final class Autos {
+  /** Example static factory for an autonomous command. */
+  // public static Command exampleAuto(ExampleSubsystem subsystem) {
+  //   return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  // }
+
+  //which way is front
+  public static String FACE = "intake";
+
+  public static Command testingAuto(Drivetrain drivetrain) {
+    return Commands.sequence(new testAutoCommand(drivetrain));
+  }
+
+  public static Command autoMin(Shooter shooter, Intake intake, Drivetrain drivetrain) {
+    return new SequentialCommandGroup(
+      new doNothing(),
+      new autoShootCommand(shooter, intake),
+      new driveOutStartCommand(drivetrain)
+    );
+  }  
+
+  
+  //select an autonomous command
+  public static Command selectAuto(Shooter shooter, Intake intake, Drivetrain drivetrain) {
+    String command = SmartDashboard.getString("Auto Selector", "testingAuto");
+    if (command == "autoMin") {
+      return autoMin(shooter, intake, drivetrain);
+    } else {
+      return testingAuto(drivetrain);
+    }
+  } 
+
+
+  private Autos() {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
+}
---
+++ robot code/everythingSoFar/2.24.24/subsystems/motors/Intake.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.motors;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  /** Creates a new Intake. */
+
+  //ids for intake parts - limit switch and motor
+  private static final int intakeMotorID = 9;
+
+  //make intake motor
+  CANSparkMax m_intake;
+
+  //make a limit switch - zero because we said so
+  public final DigitalInput limitSwitch = new DigitalInput(0);
+
+  public Intake() {
+    //initiate motor
+    m_intake = new CANSparkMax(intakeMotorID, MotorType.kBrushless);
+  }
+
+  public void startIntake() {
+    //intake at 100% speed
+    m_intake.set(1.0);
+  }
+
+  public void stopIntake() {
+    //intake at 0% speed
+    m_intake.set(0.0);
+  }
+
+  public void intakeReverse() {
+    //intake at -60% speed
+    m_intake.set(-0.6);
+  }
+
+  public void shootIntake(){
+    //intake go brrrrrr - right now it's at 60%
+    m_intake.set(0.6);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
